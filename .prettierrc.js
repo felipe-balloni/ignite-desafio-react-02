aa222795149be37d9d291dfced61ca8f24f9d15e
Felipe Balloni Ferreira <felipe.balloni@hotmail.com>
2022-06-26 16:08:55 -0300

    Desafio concluído

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'none',
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always'
+};
